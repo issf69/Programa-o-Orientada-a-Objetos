ed2b08c640cfaa89338d718e8c94e5448cbbcd53
Irene <irenesilvafranca2016@gmail.com>
2022-12-03 09:52:36 -0300

    first commit

---
+++ src/dio/me/oo/Classe2.java
@@ -0,0 +1,17 @@
+package dio.me.oo;
+
+import dio.me.oo.Classe1;
+
+class Classe2 extends Classe1 {
+
+    //Atributos próprios
+
+    void metodo() {
+
+        //atributo2 e 3
+
+    }
+
+    //metodo2 e 3
+
+}
---
+++ src/outro/pacote/Classe4.java
@@ -0,0 +1,17 @@
+package outro.pacote;
+
+import dio.me.oo.Classe1;
+
+class Classe4 {
+
+    Classe1 classe1;
+
+    void metodo() {
+
+        //atributo3
+
+        //metodo3
+
+    }
+
+}
